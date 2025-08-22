396807d19076012de3ced961e45d7fb8fb9d1b05
Mathortas <elderiva83@gmail.com>
2025-08-22 15:28:50 -0500

    Update Buscaminas 1.cpp

--- Buscaminas 1.cpp
+++ Buscaminas 1.cpp
@@ -2,51 +2,54 @@
 using namespace std;
 
 int main() {
-    int tablero[10][10] = {
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,-1,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,-1,0,0,0,0,0,-1},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,-1,0,0,0},
-        {0,0,0,0,0,0,-1,0,-1,-1},
-        {0,0,0,0,0,0,0,0,0,0}
-    };
+	int tablero[10][10] = {
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,-1,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,-1,0,0,0,0,0,-1},
+		{0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,-1,0,0,0},
+		{0,0,0,0,0,0,-1,0,-1,-1},
+		{0,0,0,0,0,0,0,0,0,0}
+	};
 
-    int *base = *tablero;
-    int *fin  = base + 100;
+	int *base = *tablero;
+	int *fin  = base + 100;
 
+	int marcas[8] = {-11, -10, -9, -1, 1, 9, 10, 11};
 
-    for (int *p = base; p < fin; p++) {
-        if (*p < 0) {
-            int marcas[8] = {-11, -10, -9, -1, 1, 9, 10, 11};
-            for (int k = 0; k < 8; k++) {
-                int *vecino = p + marcas[k];
+	for (int *p = base; p < fin; p++) {
+		if (*p < 0) {
+			int col = (p - base) % 10;
+			for (int* mar = marcas; mar < marcas+8; mar++) {
                 
-                if ((p - base) % 10 == 0 && (marcas[k] == -1 || marcas[k] == -11 || marcas[k] == 9))
-                    continue;
-                    
-                if ((p - base) % 10 == 9 && (marcas[k] == 1 || marcas[k] == -9 || marcas[k] == 11))
-                    continue;
-                    
-                if (*vecino >= 0) 
-                (*vecino)++;
-            }
-        }
-    }
+                //Columna izquierda
+				if (col == 0 && (*(mar) == -1 || *(mar) == -11 || *(mar) == 9))
+					continue;
+				//Columna derecha
+				if (col == 9 && (*(mar) == 1 || *(mar) == -9 || *(mar) == 11))
+					continue;
 
-    // Imprimir resultado
-    int contador = 0;
-    
-    for (int *p = base; p < fin; p++) {
-        if (*p < 0) cout << "* ";
-        else cout << *p << " ";
-        contador++;
-        if (contador == 10) { 
-            cout << endl; 
-            contador = 0; 
-        }
-    }
+				p += *(mar);
+				
+				if (*p >= 0)
+					(*p)++;
+				
+				p -= *(mar);
+			}
+		}
+	}
+	
+	int contador = 0;
+	for (int *p = base; p < fin; p++) {
+		if (*p < 0) cout << "* ";
+		else cout << *p << " ";
+		contador++;
+		if (contador == 10) {
+			cout << endl;
+			contador = 0;
+		}
+	}
 }
